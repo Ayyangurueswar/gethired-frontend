23f7d56908404f03cbfe44e18ed8e35b41879c13
Ayyangurueswar <ayyan16.gbps@gmail.com>
2024-06-02 20:38:02 +0530

    authentication pages

---
+++ app/account/signup/candidate/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+import Link from 'next/link'
+import { useState } from 'react';
+
+const Page = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+        <div className='w-1/3 flex flex-col h-5/6 justify-between'>
+            <h1 className='text-center text-4xl font-bold'>Signup</h1>
+            <form className='flex flex-col gap-4'>
+                <div className='flex flex-col'>
+                  <label htmlFor='name' className='mb-2'>Name</label>
+                  <input id='name' className='border border-slate-500 rounded-md px-4 py-2' value={name} onChange={(e) => {setName(e.target.value)}}/>
+                </div>
+                <div className='flex flex-col'>
+                  <label htmlFor='email' className='mb-2'>Email</label>
+                  <input type='email' id='email' className='border border-slate-500 rounded-md px-4 py-2' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+                </div>
+                <div className='flex flex-col'>
+                  <label htmlFor='password' className='mb-2'>Password</label>
+                  <input type='password' id='password' className='border border-slate-500 rounded-md px-4 py-2' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+                </div>
+                <div className='flex flex-col'>
+                  <label htmlFor='confirmPwd' className='mb-2'>Confirm Password</label>
+                  <input type='password' id='confirmPwd' className='border border-slate-500 rounded-md px-4 py-2' value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}}/>
+                </div>
+            </form>
+            <div className='w-full flex flex-col items-center gap-3'>
+                <button className='bg-slate-700 text-white rounded-md px-4 py-2 w-1/3'>Signup</button>
+                <p className='text-sm'>Already have an account? <Link href='/account/login/candidate' className='underline text-blue-500'>Sign in</Link></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Page
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+'use client';
+import React, {useState} from 'react'
 import Link from 'next/link'
-import { MotionValue, motion } from 'framer-motion'
+import { AnimatePresence, MotionValue, motion } from 'framer-motion'
 
 const Header = ({progress}: {
     progress: MotionValue<number>
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [signupOpen, setSignupOpen] = useState(false);
   return (
     <>
         <div className="w-full px-14 py-4 flex items-center justify-between fixed bg-white z-10">
@@ -15,8 +18,32 @@ const Header = ({progress}: {
                 <Link href='/'>For companies</Link>
             </div>
             <div className="flex items-center gap-4">
-                <Link href='/' className="px-4 py-2 border border-slate-500 rounded-lg">Login</Link>
-                <Link href='/' className="px-4 py-2 bg-slate-800 text-white rounded-lg">Sign up</Link>
+                <div className='relative'>
+                    <button className="px-4 py-2 border border-slate-500 rounded-lg" onClick={() => {setIsOpen(!isOpen)}}>Login</button>
+                    <AnimatePresence>
+                        {
+                            isOpen && (
+                                <motion.div className='absolute top-12 flex flex-col w-32 border rounded-md border-slate-600 overflow-hidden' initial={{y: -100}} animate={{y: 0}} exit={{y: -200}}>
+                                    <Link href='/account/login/candidate' className='w-full text-center border-b border-slate-500 p-2 hover:bg-blue-500 hover:text-white transition-colors'>Job seeker</Link>
+                                    <Link href='/account/login/recruiter' className='w-full text-center hover:bg-blue-500 hover:text-white transition-colors p-2'>Job poster</Link>
+                                </motion.div>
+                            )
+                        }
+                    </AnimatePresence>
+                </div>
+                <div className='relative'>
+                    <button className="px-4 py-2 bg-slate-800 text-white rounded-lg" onClick={() => {setSignupOpen(!signupOpen)}}>Sign up</button>
+                    <AnimatePresence>
+                        {
+                            signupOpen && (
+                                <motion.div className='absolute top-12 flex flex-col w-32 border rounded-md border-slate-600 overflow-hidden' initial={{y: -100}} animate={{y: 0}} exit={{y: -200}}>
+                                    <Link href='/account/signup/candidate' className='w-full text-center border-b border-slate-500 p-2 hover:bg-blue-500 hover:text-white transition-colors'>Job seeker</Link>
+                                    <Link href='/account/signup/recruiter' className='w-full text-center hover:bg-blue-500 hover:text-white transition-colors p-2'>Job poster</Link>
+                                </motion.div>
+                            )
+                        }
+                    </AnimatePresence>
+                </div>
             </div>
         </div>
         <motion.div className='h-2 bg-slate-900 fixed top-0 left-0 right-0 z-10' style={{scaleX: progress, transformOrigin: '0%'}}></motion.div>
